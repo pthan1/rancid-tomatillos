4447c5450b468c3cdc36c897b3563040da18a171
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-10-16 13:53:22 -0600

    Display individual movie details on selected

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,16 @@
 
 import React from 'react';
 
-const Card = ({setSelectedMovieToState, id, title, poster_path, average_rating}) => {
-  return (
-    <article className="card-container">
-      <button className="movie-card" onClick={() => setSelectedMovieToState(id)}>
-        <img src={poster_path} alt="Movie poster" />
-        <h3>{title}</h3>
-      </button>
-    </article>
-  )
-}
+const Card = ({ setSelectedMovieToState, id, title, poster_path, average_rating }) => {
+	return (
+		<article className="card-container">
+			<button className="movie-card" onClick={() => setSelectedMovieToState(id)} id={id}>
+				<img src={poster_path} alt="Movie poster" />
+				<h3>{title}</h3>
+			</button>
+		</article>
+	);
+};
 
 //
 // const Card = ()
