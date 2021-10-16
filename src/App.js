3905c53b51f6b7537d6a5d40c1e686a091065594
Joshua Horner <lesliewbusiness@gmail.com>
2021-10-16 16:17:55 -0600

    Add single movie fetch call

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import './Card.css';
 import Header from './Header.js';
 import Movies from './Movies';
 import './Movies.css';
-import './tempMoviesData';
-import movieData from './tempMoviesData';
 import MovieDetails from './MovieDetails';
 import './MovieDetails.css';
-// import movieDetail'./tempMovieDetail'
 
 class App extends Component {
 	constructor() {
@@ -23,54 +20,22 @@ class App extends Component {
 	}
 
 	componentDidMount = () => {
-		// this.setState({ movies: movieData.movies });
-
 		fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
 		.then(response => response.json())
 		.then(data => {
 			this.setState({ movies: data.movies })
 		})
-
-		// fetch('http://example.com/movies.json')
-  	// .then(response => response.json())
-  	// .then(data => console.log(data));
-		// fetch from URL _____
-		//.then that parses the response object
-		// a .then that invokes a function in utils.js that cleans up the data to what we want to display
-		// Should set state
-		// Data will reflect:
-		// id
-		// movie
-		// poster_path
-		// backdrop_path
-		// release_date
-		// overview
-		// average_rating
-		// genres
-		// id
-		// name
-		// budget
-		// revenue
-		// runtime
-		// tagline
-	};
-
-	// showSelectedMovieDetails = (id) => {
-	// 	console.log(id);
-	// 	// Should hide banner and cards container
-	// 	// Should show movie details display
-	// };
-
-	hideSelectedMovieDetails = () => {
-		this.setState({ selectedMovie: {} });
 	};
 
 	setSelectedMovieToState = (id) => {
+		fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+		.then(response => response.json())
+		.then(data => {
+			this.setState({ selectedMovie: data.movie })
+		})
 		const selectedMovie = this.state.movies.find((movie) => {
 			return movie.id === id;
 		});
-		console.log('id', id);
-		console.log('selectedMovie', selectedMovie);
 		this.setState({ isAMovieSelected: true, selectedMovie: selectedMovie });
 	};
 
@@ -102,15 +67,5 @@ class App extends Component {
 	}
 }
 
-// <Header />
-// two (2) HTML Sidebars // banner HTML
-// <Movies movies={this.state.movies} setSelectedMovieToState={this.setSelectedMovieToState} />
-// {!this.state.movies && <h2>Loading</h2>}
-// {this.state.selectedMovie && (
-// 	<MovieDetails
-// 		selectedMovie={this.state.selectedMovie}
-// 		hideSelectedMovieDetails={this.hideSelectedMovieDetails}
-// 	/>
-// )}
 
 export default App;
