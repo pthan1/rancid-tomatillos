bd876645cd64fb970a59c972ae6c3a36e00c5c17
Joshua Horner <lesliewbusiness@gmail.com>
2021-10-21 16:55:24 -0600

    Style Movie Details view

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,53 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class MovieDetails extends Component {
-	// ({ selectedMovie, unsetSelectedMovieFromState, selectedMovieId }) => {
-	constructor() {
-		super();
-		this.state = {
-			selectedMovie: {}
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      selectedMovie: {},
+    };
+  }
 
-	componentDidMount = () => {
-		console.log(this.props);
-		fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.selectedMovieId}`)
-			.then((response) => {
-				if (response.ok) {
-					return response.json();
-				}
-				throw new Error('Something went wrong. Please try again.');
-			})
-			.then((data) => {
-				this.setState({ selectedMovie: data.movie });
-			})
-			.catch((error) => this.setState({ error: error.toString() }));
-	};
+  componentDidMount = () => {
+    console.log(this.props);
+    fetch(
+      `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.selectedMovieId}`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Something went wrong. Please try again.");
+      })
+      .then((data) => {
+        this.setState({ selectedMovie: data.movie });
+      })
+      .catch((error) => this.setState({ error: error.toString() }));
+  };
 
-	render() {
-		return (
-			<div className="movie-details-container">
-				<section className="movie-details">
-					<img src={this.state.selectedMovie.backdrop_path} alt="still from a movie" />
-					<p>{this.state.selectedMovie.title}</p>
-					<p>{this.state.selectedMovie.revenue}</p>
-					<div className="return-button-container">
-						<Link to="/">
-							<button
-								className="return-to-main-button"
-								id="returnToMain"
-								// onClick={unsetSelectedMovieFromState}
-							>
-								Return to main
-							</button>
-						</Link>
-					</div>
-				</section>
-			</div>
-		);
-	}
+  render() {
+    let movieRating = Math.round(this.state.selectedMovie.average_rating);
+    let movieGenres = this.state.selectedMovie.genres;
+    return (
+      <div className="movie-details-container">
+        <div class="backdrop-container">
+          <img
+            className="movie-backdrop"
+            src={this.state.selectedMovie.backdrop_path}
+            alt="still from a movie"
+          />
+        </div>
+        <span class="movie-information-background"></span>
+        <section className="movie-information">
+          <div className="information-elements">
+            <h1 class="movie-title">{this.state.selectedMovie.title}</h1>
+            <p className="movie-genres">{movieGenres}</p>
+            <p>
+              Average rating: <span class="rating-number">{movieRating}</span>
+            </p>
+            <p className="movie-overview">
+              {this.state.selectedMovie.overview}
+            </p>
+            <div className="return-button-container">
+              <Link to="/">
+                <button className="return-to-main-button" id="returnToMain">
+                  Return to main
+                </button>
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default MovieDetails;
