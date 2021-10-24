61a6cab276df0e8623f9717e2db67827073ac64f
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-10-24 14:11:26 -0600

    Style average rating on cards

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,9 @@ const Card = ({ id, title, poster_path, average_rating }) => {
 				<button className="movie-card" id={id}>
 					<img src={poster_path} alt="Movie poster" />
 					<h3>{title}</h3>
-					<h2 className="average-rating">{average_rating}</h2>
+					<div className="average-rating-container">
+						<h2 className="average-rating">{average_rating}</h2>
+					</div>
 				</button>
 			</article>
 		</Link>
