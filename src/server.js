cbe946196f9dae2803ef6446dd4cf411a37ecd8f
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-10-23 19:11:33 -0600

    Add rating delete request

--- src/server.js
+++ src/server.js
@@ -21,8 +21,14 @@ app.get('/api/v1/ratings', (request, response) => {
 });
 
 app.post('/api/v1/ratings', (request, response) => {
+	const body = request.body;
 	const rating_id = Date.now();
 	const { movie_id, rating, user_id } = request.body;
+
+	if (!body.movie_id || !body.rating || body.user_id) {
+		return response.status(400).json('Movie ID, rating, and user id are required');
+	}
+
 	app.locals.ratings.push({ rating_id, movie_id, rating, user_id });
 	response.status(201).json({ rating_id, movie_id, rating, user_id });
 });
@@ -45,7 +51,20 @@ app.patch('/api/v1/ratings/:rating_id', (request, response) => {
 		rating.rating = body.rating;
 	}
 	console.log('after', app.locals.ratings);
-	response.json(rating);
+	response.status(201).json(rating);
+});
+
+app.delete('/api/v1/ratings/:rating_id', (request, response) => {
+	const body = request.body;
+	const rating = app.locals.ratings.find((rating) => rating.rating_id === parseInt(request.params.rating_id));
+
+	if (!rating) {
+		return response.status(404).json('Rating not found');
+	}
+
+	app.locals.ratings = app.locals.ratings.filter((rating) => rating.rating_id !== parseInt(request.params.rating_id));
+
+	response.status(204);
 });
 
 app.listen(app.get('port'), () => {
