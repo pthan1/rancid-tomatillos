866f25e311a90c2474d14905f19e3c60caff71c3
Joshua Horner <lesliewbusiness@gmail.com>
2021-10-24 12:30:31 -0600

    Display rating on main page cards

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Card = ({ id, title, poster_path }) => {
+const Card = ({ id, title, poster_path, average_rating }) => {
 	return (
 		<Link to={`/movies/${id}`}>
 			<article className="card-container">
 				<button className="movie-card" id={id}>
 					<img src={poster_path} alt="Movie poster" />
 					<h3>{title}</h3>
+					<h2 className="average-rating">{average_rating}</h2>
 				</button>
 			</article>
 		</Link>
